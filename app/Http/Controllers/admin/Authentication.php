dabb4bff3677e6053b20a65956e53537f699d789
krunalpatel-oo3 <59408769+krunalpatel-oo3@users.noreply.github.com>
2023-12-15 22:01:08 +0530

    PWA test

--- app/Http/Controllers/admin/Authentication.php
+++ app/Http/Controllers/admin/Authentication.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Hash;
 use Auth;
-use Kutia\Larafirebase\Facades\Larafirebase;
+// use Kutia\Larafirebase\Facades\Larafirebase;
 
 class Authentication extends Controller
 {
@@ -45,18 +45,57 @@ class Authentication extends Controller
     }
 
     public function sendNotification(Request $request){
-        $url = 'https://fcm.googleapis.com/fcm/send';
+        // $url = 'https://fcm.googleapis.com/fcm/send';
 
-        // $FcmToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+        // // $FcmToken = User::whereNotNull('device_token')->pluck('device_token')->all();
             
         $serverKey = 'AAAA9s-awdI:APA91bFd491JgFr4dn12HW2lQJI2s92rxGQb7EfQAfPxT2qzK27KQScqF-WyL5UY2p1IL8ngupwkiCXX9wsJgZNk5awjcsdxtQYO36fdGXX4T537o9Z-cAW6oXEGE4F9UW5hTnUAHHqI'; // ADD SERVER KEY HERE PROVIDED BY FCM
-        $FcmToken = 'eOnUUro4f2u5cG_9MMM9WN:APA91bG0yfGTpCfFsjppgnZFUVaMe-7Gtkq842cgKWT562myyiGoS0uYI2tgmNdmpl712J1zP5oDo_oH_w3KUtwxe-lRTGxdxcIXasejyPP55S6QD_7XiZggZQ8Fv5Rykwdwk9ShGBfS';
+        $FcmToken = ['enb-PIM7USBIqvXAjxMQ-N:APA91bEG1eWO2aHDHIvXEvlD_K-spalTNlBuc6GygqlRBe5o-GBLpufGrTSKa7KDeMo2XJOVFEupi48UBbCZOCcW3MwvVe9eC7ZtBJS_EEcz-B-6xoDWkvKuSg4OjZVYxVY-9eF1pMkR'];
         
-        $result = Larafirebase::withTitle('YSJHA')
-            ->withBody('TEWSTVBS ABS AHGSBH')
-            ->sendMessage($FcmToken);
+        // $result = Larafirebase::withTitle('YSJHA')
+        //     ->withBody('TEWSTVBS ABS AHGSBH')
+        //     ->sendMessage($FcmToken);
 
+        // // FCM response
+        // dd($result);
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        // $FcmToken = User::whereNotNull('device_key')->pluck('device_key')->all();
+          
+        // $serverKey = 'server key goes here';
+  
+        $data = [
+            "registration_ids" => $FcmToken,
+            "notification" => [
+                "title" => 'DSASDAA',
+                "body" => 'SABSJHABJHSBJS',  
+            ]
+        ];
+        $encodedData = json_encode($data);
+    
+        $headers = [
+            'Authorization:key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+    
+        $ch = curl_init();
+      
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        // Execute post
+        $result = curl_exec($ch);
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }        
+        // Close connection
+        curl_close($ch);
         // FCM response
-        dd($result);
+        dd($result);    
     }
 }
